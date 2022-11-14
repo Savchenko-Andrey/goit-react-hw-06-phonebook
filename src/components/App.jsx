b1d1a6c0a3ae0acf42087d2bdfda58f0899e742d
Savchenko-Andrey <dracxm95@gmail.com>
2022-11-14 16:41:37 +0200

    Revert "change"
    This reverts commit 92db779e755a8e70d68e323400a9ae732015fd9d.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,16 @@
-import { Box } from 'commonStyle/Common.styled';
-import { Formes } from './FormSection/Form';
-import { Filter } from './FilterSection/Filter';
-import { Renderlist } from './ListContact/ListContact';
-import { ContactList } from './ListContact/ListContact.styled';
-
-export const Phonebook = () => {
+export const App = () => {
   return (
-    <>
-      <Box display="flex" flexDirection="column" alignItems="center" p={8}>
-        <h1>Phonebook</h1>
-        <Formes />
-        <h2>Contacts</h2>
-        <Filter />
-        <ContactList>
-          <Renderlist />
-        </ContactList>
-      </Box>
-    </>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
-import { UserProvider } from 'components/CastomHook/Provider';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, presistor } from 'redux/store';
-import { Phonebook } from 'components/App';
-import { theme } from 'Thema';
+import { App } from 'components/App';
 import './index.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={presistor}>
-        <UserProvider>
-          <ThemeProvider theme={theme}>
-            <Phonebook />
-          </ThemeProvider>
-        </UserProvider>
-      </PersistGate>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
